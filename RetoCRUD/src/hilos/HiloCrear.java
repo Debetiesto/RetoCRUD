2eaa7c34f8cf50af83e088221f5aacfc4a0285e7
Debetiesto <luisvazquez8942@gmail.com>
2025-11-11 09:52:50 +0100

    Registrar con hilo

--- RetoCRUD/src/hilos/HiloCrear.java
+++ RetoCRUD/src/hilos/HiloCrear.java
@@ -5,10 +5,40 @@
  */
 package hilos;
 
+import controlador.Controlador;
+import controlador.Dao;
+import javafx.application.Platform;
+import modelo.Usuario;
+
 /**
  *
  * @author 2dam
  */
-public class HiloCrear {
-    
+public class HiloCrear implements Runnable {
+
+    private Dao dao;
+    private Controlador cont;
+    private Usuario usu;
+
+    public HiloCrear(Dao dao, Controlador cont, Usuario usu) {
+        this.dao = dao;
+        this.cont = cont;
+        this.usu = usu;
+    }
+
+    @Override
+    public void run() {
+        boolean insertado = dao.insertarUsuario(usu);
+
+        Platform.runLater(() -> {
+            if (insertado) {
+                cont.mostrarMensaje("Registro exitoso." + " Usuario registrado correctamente.");
+                cont.volverLogin();
+            } else {
+                cont.mostrarMensaje("Error, " + "no se pudo registrar el usuario.");
+            }
+        });
+
+    }
+
 }
